68c3e80cfb22f9924b7b1ea84f6649373229a057
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2022-08-25 15:42:19 +0200

    Log the start of a server

--- lib/manger/cli.ex
+++ lib/manger/cli.ex
@@ -3,6 +3,8 @@ defmodule Manger.CLI do
   An entry point for Manger escript
   """
 
+  require Logger
+
   def main(argv) do
     {options, _argv_rest} =
       OptionParser.parse!(argv,
@@ -13,8 +15,13 @@ defmodule Manger.CLI do
         aliases: [d: :dir, p: :port]
       )
 
+    options = Keyword.put_new(options, :port, 8000)
+
     Application.put_all_env(manger: options)
     Application.ensure_all_started(:manger)
+
+    port = options[:port]
+    Logger.info("Started HTTP server at port #{port}. Visit http://localhost:#{port}")
 
     Process.sleep(:infinity)
   end
